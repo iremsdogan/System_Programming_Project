e0f016d22f1ac0fa2583045e4e546828dcc3a150
İrem Sevdenur Doğan <iremsdogann@gmail.com>
2024-12-29 01:26:11 +0300

    second commit

--- src/include/directory_ops.h
+++
@@ -1,9 +0,0 @@
-#ifndef DIRECTORY_OPS_H
-#define DIRECTORY_OPS_H
-
-void list_directory(const char *path);
-void create_directory(const char *path);
-void remove_directory(const char *path);
-void remove_directory_with_content(const char *path);
-
-#endif // DIRECTORY_OPS_H
--- src/include/permissions.h
+++
@@ -1,8 +0,0 @@
-#ifndef PERMISSIONS_H
-#define PERMISSIONS_H
-
-#include <sys/types.h>  // for mode_t type
-
-void change_permissions(const char *path, mode_t mode);
-
-#endif // PERMISSIONS_H
